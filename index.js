849a2dc940da60d6a3001579f20bb9c4ac123364
shivanipremi <shivanipremi00@gmail.com>
2019-04-07 16:34:24 +0530

    changed port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
-let port = process.env.port || 3000;
 app.get("/", (req, res) => {
   res.send({ bye: "buddy" });
 });
+
+let port = process.env.PORT || 5000;
+
 
 app.listen(port);
